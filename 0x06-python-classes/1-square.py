39b93f8fa537e5e0264952ee224f69644729d51a
Zachariarian <zachariarian@gmail.com>
2022-07-19 15:16:28 +0300

    japelk

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
-""" Working on oop with python """
-class Square():
-    """Class representing a square"""
+""" Module contains: class Square """
+
+
+class Square:
+    """
+        Square: defines a square
+        Attributes:
+            size (no type or value identification): size of square
+        Method:
+            __init__: init of size attribute for each instance
+    """
+
     def __init__(self, size):
-        """ initialize the class
-        Args:
-           size (int): size of the square
         """
+            Initialization of attributes for all instances
+            Args:
+                size (no type): size of the square
+        """
+
         self.__size = 
