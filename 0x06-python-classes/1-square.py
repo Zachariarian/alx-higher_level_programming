5170636b5e32c9baf57ed33ad9cfaaa6d0c6b30d
Zachariarian <zachariarian@gmail.com>
2022-07-20 10:57:20 +0300

    Pycodestyle validation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,13 +8,4 @@ class Square:
     """Square implementation
     """
     def __init__(self, size):
-<<<<<<< HEAD
-        """ initialize the class
-        Args:
-           size (int): size of the square
-        """
-        self.__size = 
-
-=======
         self.__size = size
->>>>>>> 53a4c94dc85f6c09834e24f37357e7211e216cdb
