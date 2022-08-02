fdd8b7377d5205d2de26809c01fc8b199eb7adb9
Zachariarian <zachariarian@gmail.com>
2022-08-02 16:45:21 +0300

    0x0B-python-input_output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-"""
-Contains the class "Student"
-"""
+""" Student to JSON """
 
 
 class Student:
-    """Representation of a student"""
+    """ New class student """
+
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """returns a dictionary representation of a Student instance
-        with specified attributes"""
         if attrs is None:
-            return self.__dict__
-        new_dict = {}
-        for a in attrs:
-            try:
-                new_dict[a] = self.__dict__[a]
-            except:
-                pass
-        return new_dict
+            return (self.__dict__)
+        else:
+            dic = {}
+            for nm in attrs:
+                if hasattr(self, nm):
+                    dic[nm] = getattr(self, nm)
+            return (dic)
--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,34 +1,28 @@
 #!/usr/bin/python3
-"""
-Contains the class "Student"
-"""
+""" Student to JSON """
 
 
 class Student:
-    """Representation of a student"""
+    """ New class student """
+
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """returns a dictionary representation of a Student instance
-        with specified attributes"""
+        """ Retrieves a dictionary representation of a Student instance """
         if attrs is None:
-            return self.__dict__
-        new_dict = {}
-        for a in attrs:
-            try:
-                new_dict[a] = self.__dict__[a]
-            except:
-                pass
-        return new_dict
+            return (self.__dict__)
+        else:
+            dic = {}
+            for nm in attrs:
+                if hasattr(self, nm):
+                    dic[nm] = getattr(self, nm)
+            return (dic)
 
     def reload_from_json(self, json):
-        """replaces all attributes of the Student instance"""
-        for key in json:
-            try:
-                setattr(self, key, json[key])
-            except:
-                pass
+        """ Replaces all attributes of the Student instance"""
+        save = vars(self)
+        for key, value in json.items():
+            save[key] = value
