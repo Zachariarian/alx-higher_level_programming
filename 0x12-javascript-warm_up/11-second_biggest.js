ec4d062f234fdef5866e655589c6711e863bc112
Zachariarian <zachariarian@gmail.com>
2022-10-03 17:51:20 +0300

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
 if (process.argv.length > 3) {
-    const array = process.argv.slice(2).map(Number);
+  const array = process.argv.slice(2).map(Number);
   
-    array.splice(array.indexOf(Math.max.apply(null, array)), 1);
-    console.log(Math.max.apply(null, array));
-  } else {
-    console.log(0);
-  }
-  
+  array.splice(array.indexOf(Math.max.apply(null, array)), 1);
+  console.log(Math.max.apply(null, array));
+} else {
+  console.log(0);
+}
